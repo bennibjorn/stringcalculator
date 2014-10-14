c991940154b1917550542c309fafd4e074147aa3
Benjamin Hinriksson <bennibjorn@hotmail.com>
2014-10-14 18:13:10 +0000

    Wrote tests in previous commit for 4th kata step - code now, 4th step solved

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,10 +1,16 @@
 package is.ru.stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
 	public static int add(String text){
 		if(text.equals("")){
 			return 0;
+		}
+		else if(text.startsWith("//")) {
+			return sum(specificDelimiter(text));
 		}
 		else if(text.contains(",") || text.contains("/n")){
 			return sum(splitNumbers(text));
@@ -12,6 +18,15 @@ public class Calculator {
 		else
 			return toInt(text);
 	}
+    // If there is a specific delimiter, determine the delimiter using regex
+    // and split using that specific delimiter
+    private static String[] specificDelimiter(String specific) {        
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(specific);
+        m.matches();
+        String delimiter = m.group(1);
+        String numbers = m.group(2);
+        return numbers.split(delimiter);
+    }
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
